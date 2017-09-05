817c3ba77af9131a109b77a4f50e4c53070d7f54
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-09-06 01:04:55 +0800

    demo-meteor list sort

--- demo-meteor/imports/ui/App.jsx
+++ demo-meteor/imports/ui/App.jsx
@@ -63,7 +63,8 @@ App.propTypes = {
 
 export default createContainer(() => {
   return {
-    tasks: Tasks.find({}).fetch()
+    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch()
+
   };
 }, App);
 
