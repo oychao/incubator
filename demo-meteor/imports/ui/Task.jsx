6304328b4d96f619198cd2da0a989ce1243f9489
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-09-06 23:17:04 +0800

    demo-meteor api/ui/Task

--- demo-meteor/imports/ui/Task.jsx
+++ demo-meteor/imports/ui/Task.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 
+import { Tasks } from '../api/tasks';
+
 class Task extends React.Component {
+  constructor() {
+    super();
+    this.toggleChecked = this.toggleChecked.bind(this);
+    this.deleteThisTask = this.deleteThisTask.bind(this);
+  }
+
+  toggleChecked() {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked }
+    });
+  }
+
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    const checked = !!this.props.task.checked;
+    const taskClassName = checked ? 'checked' : '';
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask}>&times;</button>
+        <input type="checkbox" readOnly checked={checked}
+          onClick={this.toggleChecked}/>
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
