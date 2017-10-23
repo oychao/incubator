a7bb22a8d74deae6b7244a5ff8ac86133c5b2186
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-10-23 19:32:15 +0800

    demo-react-ts configuring

---
+++ demo-react-ts/src/index.ts
@@ -0,0 +1,2 @@
+
+document.body.innerHTML = 'Hello Webpack, TypeScript, Less';
--- demo-react-ts/webpack.config.js
+++ demo-react-ts/webpack.config.js
@@ -1,43 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            {
-                test: /\.tsx?$/,
-                use: [{
-                    loader: "awesome-typescript-loader"
-                }]
-            },
-
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                use: "source-map-loader"
-            }
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: [{
+        loader: 'awesome-typescript-loader'
+      }]
+    }, {
+      enforce: 'pre',
+      test: /\.js$/,
+      use: [{
+        loader: 'source-map-loader'
+      }]
+    }, {
+      test: '/\.less$/',
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader'
+      }, {
+        loader: 'less-loader'
+      }]
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hello World'
+    })
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist'
+  }
 };
