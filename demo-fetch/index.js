deffffa344e738e1e91008f97cb73b587ac737f5
Ouyang Chao <ch.k.ouyang@gmail.com>
2017-11-25 23:45:18 +0800

    demo-rxjs throttleTime

--- demo-fetch/index.js
+++ demo-fetch/index.js
@@ -3,14 +3,24 @@ const app = express();
 
 app.use(express.static(__dirname + '/html'));
 
-app.get('/data', function(req, res) {
+app.get('/data', function (req, res) {
   res.send('hello fetch');
 });
 
-app.post('/datap', function(req, res) {
+app.post('/datap', function (req, res) {
   console.log(req.params);
+  const body = '';
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+  req.on('end', function () {
+    const data = qs.parse(body);
+    res.writeHead(200);
+    res.end(JSON.stringify(data));
+  });
+  res.send('end');
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('server started on port 3000');
 });
--- demo-rxjs/src/index.js
+++ demo-rxjs/src/index.js
@@ -1,14 +1,12 @@
-// import { Observable } from 'rxjs';
-import { Observable } from './Observable';
+import { Observable } from 'rxjs';
+// import { Observable } from './Observable';
 
 const btn = document.createElement('button');
 btn.textContent = 'Click me and check the console!';
 document.body.appendChild(btn);
 
-
 Observable.fromEvent(btn, 'click')
+  .throttleTime(500)
   .subscribe(e => {
     console.log('hello world')
   });
-
-console.log('test');
