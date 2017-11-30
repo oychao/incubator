7c5f87c350993aa90fe47ea9ef18fc5b02320118
oychao <ch.k.ouyang@gmail.com>
2017-11-30 23:11:51 +0800

    demo-echarts

---
+++ demo-echarts/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve('dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hello Echarts',
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    })
+  ],
+  externals: {
+    echarts: 'echarts'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  }
+};
